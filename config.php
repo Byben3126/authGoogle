2fde3754b2032c7af98f2bb5acbca2e6df61b196
Byben3126 <74027051+Byben3126@users.noreply.github.com>
2023-10-12 16:33:01 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-Define("GOOGLE_KEY_PRIVATE","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");
-Define("GOOGLE_ID_CLIENT","900162052472-k22gtqgtvt4onqagalbl967qhss160js.apps.googleusercontent.com")
+Define("GOOGLE_KEY_PRIVATE","xxxx");
+Define("GOOGLE_ID_CLIENT","xxxx)
 
 ?>
